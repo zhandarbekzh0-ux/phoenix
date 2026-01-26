ba26b59c13897085f4ad488c558e91f22c76d1b4
zhandarbekzh0-ux <zhandarbekzh0@gmail.com>
2026-01-27 01:24:26 +0500

    Added files for topics HOME, Get Started, Syntax

---
+++ HOME/hello_world.py
@@ -0,0 +1,11 @@
+print("Hello world") #Plain text
+
+print("Python is fun!") #Text with a few words
+
+print(3.14)
+print(234) #Print integers
+
+print("Hello" + " " + "World") #String concatenation
+
+name = "Zhandarbek"
+print("My name is " + name)
